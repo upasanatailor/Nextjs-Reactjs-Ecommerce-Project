71a8e7dc15c4b4f9d7fa2fd929b1bda3091a15bf
upasana tailor <upasanatailor90@gmail.com>
2022-07-10 23:54:13 +0200

    fix buils

--- src/modules/FeaturedProduct/index.js
+++ src/modules/FeaturedProduct/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styles from "./FeaturedProduct.module.css";
-import FeaturedProductData from "../../component/FeaturedProductdata";
+import FeaturedProductData from "../../component/FeaturedProductData";
 import axios from "axios";
 
 const FeaturedProduct = () => {
